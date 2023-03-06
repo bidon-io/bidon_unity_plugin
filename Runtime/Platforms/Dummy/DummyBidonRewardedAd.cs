f22270f50dc3a146c952305a57cfe6e5a3817134
DF <df@bidon.org>
2023-03-06 16:14:48 +0100

    Create blank impls for other platforms (#10)

---
+++ Runtime/Platforms/Dummy/DummyBidonRewardedAd.cs
@@ -0,0 +1,51 @@
+using System;
+
+// ReSharper disable once CheckNamespace
+namespace Bidon.Mediation
+{
+    internal class DummyBidonRewardedAd : IBidonRewardedAd
+    {
+        public event EventHandler<BidonAuctionStartedEventArgs> OnAuctionStarted;
+        public event EventHandler<BidonAuctionSucceedEventArgs> OnAuctionSucceed;
+        public event EventHandler<BidonAuctionFailedEventArgs> OnAuctionFailed;
+        public event EventHandler<BidonRoundStartedEventArgs> OnRoundStarted;
+        public event EventHandler<BidonRoundSucceedEventArgs> OnRoundSucceed;
+        public event EventHandler<BidonRoundFailedEventArgs> OnRoundFailed;
+        public event EventHandler<BidonAdLoadedEventArgs> OnAdLoaded;
+        public event EventHandler<BidonAdLoadFailedEventArgs> OnAdLoadFailed;
+        public event EventHandler<BidonAdShownEventArgs> OnAdShown;
+        public event EventHandler<BidonAdShowFailedEventArgs> OnAdShowFailed;
+        public event EventHandler<BidonAdClickedEventArgs> OnAdClicked;
+        public event EventHandler<BidonAdClosedEventArgs> OnAdClosed;
+        public event EventHandler<BidonAdExpiredEventArgs> OnAdExpired;
+        public event EventHandler<BidonAdRevenueReceivedEventArgs> OnAdRevenueReceived;
+        public event EventHandler<BidonUserRewardedEventArgs> OnUserRewarded;
+
+        internal DummyBidonRewardedAd(string placement) { }
+
+        public void Load(double priceFloor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsReady()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Show()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Destroy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetPlacementId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Runtime/Platforms/Editor/EditorBidonSdk.cs
@@ -0,0 +1,55 @@
+using System;
+
+// ReSharper disable once CheckNamespace
+namespace Bidon.Mediation
+{
+    internal class EditorBidonSdk : IBidonSdk
+    {
+        public event EventHandler<BidonInitializationEventArgs> OnInitializationFinished;
+
+        public void SetLogLevel(BidonLogLevel logLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetBaseUrl(string baseUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterDefaultAdapters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterAdapter(string className)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(string appKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSdkVersion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BidonLogLevel GetLogLevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetBaseUrl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsInitialized()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
