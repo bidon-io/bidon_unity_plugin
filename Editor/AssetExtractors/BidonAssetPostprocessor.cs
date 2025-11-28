df586f1625ca55b07bceabbb83fe7d07465095b4
DF <df@bidon.org>
2025-11-28 11:45:37 +0100

    Remove obsolete conditional compilation directive

--- Editor/AssetExtractors/BidonAssetPostprocessor.cs
+++ Editor/AssetExtractors/BidonAssetPostprocessor.cs
@@ -1,7 +1,4 @@
 // ReSharper disable CheckNamespace
-// ReSharper disable HeuristicUnreachableCode
-
-#pragma warning disable CS0162
 
 using System.Linq;
 using UnityEditor;
@@ -11,16 +8,8 @@ namespace Bidon.Mediation.AssetExtractors.Editor
 {
     internal class BidonAssetPostprocessor : AssetPostprocessor
     {
-#if UNITY_2021_3_OR_NEWER
         private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload)
         {
-#else
-        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
-        {
-#endif
-#if UNITY_STANDALONE && BIDON_DEV
-            return;
-#endif
             if (deletedAssets.Any(asset => asset.Contains(EditorConstants.PackageRootDirectory))) return;
 
             if (BidonAndroidLibraryInstaller.Deploy() | BidonDependenciesInstaller.Deploy())
